a0a0b3ee148a87606b8b74f7c7dbec2afad882b1
JaeAnHan <jaean1999@naver.com>
2024-06-24 16:11:17 +0900

    feat: transform virtual node to element no diffing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import {createElement, VirtualNode, VirtualNodeType} from "./virtual-node";
+
 const world = 'World';
 
 export function hello(word: string = world): string {
@@ -5,3 +7,48 @@ export function hello(word: string = world): string {
 }
 
 console.log(hello());
+
+const rootNode: VirtualNode = {
+    type: VirtualNodeType.Div,
+    attributes: {},
+    props: { },
+    key: 'k0',
+    children: []
+};
+
+for (let i = 1; i < 5; i++) {
+    if (i % 2 == 0) {
+        rootNode.children.push({
+            type: VirtualNodeType.Div,
+            attributes: {},
+            props: {},
+            key: `k${i}`,
+            children: []
+        })
+    } else {
+        rootNode.children.push({
+            type: VirtualNodeType.Text,
+            attributes: { },
+            props: { },
+            key: `k${i}`,
+            children: [],
+            textContent: `text k${i}`
+        })
+    }
+}
+
+const root = document.getElementById("root");
+
+if (root) {
+    console.log(root.attributes);
+    rootNode.attributes = Object.assign(
+        {},
+        ...Array.from(root.attributes, ({name, value}) => ({[name]: value})) as any
+    );
+
+    setTimeout(() => {
+        root.replaceWith(createElement(rootNode));
+    }, 3000);
+} else {
+    console.log('null');
+}
